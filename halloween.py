4924ae9b68f07a70fe2320a412c0b9262b4f5f76
pugmomsf <leslie@epango.com>
2015-05-13 22:01:30 -0700

    Update halloween.py

--- halloween.py
+++ halloween.py
@@ -1,6 +1,7 @@
 # I can't be bothered to think of a Hallowe'en costume so
 # can you help me generate one randomly?
 
+import random
 
 nouns = []
 adjectives = []
@@ -10,18 +11,22 @@ with open('things.txt') as f:
     # so we call strip() before adding it to our nouns list.
     for line in f:
         nouns.append(line.strip())
+       # print nouns
 
 with open('descriptors.txt') as f:
     for line in f:
         adjectives.append(line.strip())
+       # print adjectives
 
 
 def generate_costume():
 
     # pick something random from the nouns and adjectives list
 
-    noun = "lazy"
-    adj = "person"
+    noun = nouns[random.randrange(len(nouns))]
+    adj = adjectives[random.randrange(len(adjectives))]
+   # print "%s" % noun
+    # print "%s" % adj
 
     return (noun, adj)
 
@@ -29,13 +34,13 @@ def generate_costume():
 while True:
     (noun, adjective) = generate_costume()
 
-    print "You go dressed as a {} {} to the party."
+    print "You go dressed as a {} {} to the party.".format(noun,adjective)
 
     happy = raw_input("Are you happy with this choice? ")
 
     # Check if the user typed something like 'yes' or 'y' and
     # quit the program if they are happy.
-    if happy == True:
+    if happy == "yes" or happy == "y":
         exit()
     else:
         print "OK, I will choose another costume. Hold on..."
