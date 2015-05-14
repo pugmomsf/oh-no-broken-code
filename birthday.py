c2db96eb6a50d649ad8f594dec4607f26e487382
pugmomsf <leslie@epango.com>
2015-05-13 22:00:35 -0700

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -3,7 +3,7 @@ from datetime import datetime
 number_endings = {
     1: 'st',
     2: 'nd',
-    3: 'rd'
+    3: 'rd',
 }
 
 today = datetime.now()
@@ -13,22 +13,45 @@ todays_day = today.day
 # but beware! 11 => 11th, 21 => 21st, 31 => 31st
 
 # test your code by forcing todays_day to be something different
-# todays_day = 11
+
+todays_day = 9
 
 ending = 'th'
+number = 0
+
+#for date,ending in number_endings.items():
+#	print "%d has ending %s" % (date, ending)
+
+print todays_day
 
 if todays_day < 10 or todays_day > 20:
-    # x % y (mod) will give the remainder when x is divided by y
-    # -- but x needs to be an integer!
     number = todays_day % 10
-    # look up number in number_endings
-    # and if you find it as a key, put the value in ending
-    if number in number_endings:
 
-# make this print ending, not 'th'
-print "Today is the {}th".format(todays_day)
+if number in number_endings:
+	ending = number_endings[number]
+#	print "Today is the {}".format(todays_day)+"%s" % ending
+		 # % number_ending
+		 
+# if todays_day >= 10 or todays_day <= 20:
+
+print "Today is the {}".format(todays_day)+"%s" % ending
+
+
 
 birthday = int(raw_input("What day of the month is your birthday?"))
 
 # make this print the birthday, and the right ending
-print "Your birthday is on the {}th!".format(todays_day)
+ending = 'th'
+
+				
+if birthday < 10 or birthday > 20:
+    number = birthday % 10
+
+if number in number_endings:
+	ending = number_endings[number]
+	# print "Birthday is the {}".format(birthday)+"%s" % ending
+		 # % number_ending
+		 
+# if todays_day >= 10 or todays_day <= 20:
+	
+print "Birthday is the {}".format(birthday)+"%s" % ending
