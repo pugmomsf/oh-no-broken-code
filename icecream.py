e252cd1e4fd22cd14cceec6fd3aada3b64991ef9
pugmomsf <leslie@epango.com>
2015-05-13 22:02:14 -0700

    Update icecream.py

--- icecream.py
+++ icecream.py
@@ -4,8 +4,13 @@ all_flavors = ['chocolate', 'mint', 'strawberry', 'caramel', 'pecan',
                'cookie dough', 'vanilla', 'lemon']
 my_faves    = ['mint', 'caramel']
 
-
+# for item in my_list:
+#    if my_faves in all_flavors:
+#        print "I like {}".format(item)
+        
 for item in my_faves:
-    if my_faves:
+    if item in all_flavors:
         print "I like {}".format(item)
-
+        
+     
+        
